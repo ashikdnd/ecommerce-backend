39236ee9c9a7fb4844a553ff11ecd03b299e0aba
Ashik Basheer <ashikdnd@gmail.com>
2022-11-19 13:20:08 +0530

    Handled error

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -7,17 +7,26 @@ export default class AuthController {
   async signUp({ request, response }) {
     const params = request.all()
     const hashedPassword = await Hash.make(params.password)
-    const payload = {
-      first_name: params.first_name,
-      last_name: params.last_name,
-      email: params.email,
-      password: hashedPassword,
+
+    params.password = hashedPassword
+    try {
+      const user = await Database.insertQuery().table('users').insert(params)
+      const userInfo = await Database.from('users').where('id', user).select('*')
+      response.json({
+        success: true,
+        userInfo: userInfo,
+      })
+    } catch (e) {
+      console.log(e)
+      const payload = {
+        err_code: e.sqlState,
+        err_msg: e.sqlMessage,
+      }
+      await Database.insertQuery().table('system_logs').insert(payload)
+      response.json({
+        success: false,
+        error: e.sqlMessage,
+      })
     }
-    const user = await Database.insertQuery().table('users').insert(payload)
-    const userInfo = await Database.from('users').where('id', user).select('*')
-    response.json({
-      success: true,
-      userInfo: userInfo,
-    })
   }
 }
